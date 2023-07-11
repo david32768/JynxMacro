20b399cf69dd2312fa669df5479313b944359dbd
david32768 <81751257+david32768@users.noreply.github.com>
2023-07-11 18:20:26 +0100

    Add files via upload
    Update module info.

--- src/module-info.java
+++ src/module-info.java
@@ -1,4 +1,25 @@
 module com.github.david32768.jynxmacro {
 	requires com.github.david32768.jynx;
-	provides jynx2asm.ops.MacroLib with jynxmacro.WasmMacroLib, jynxmacro.StructuredMacroLib;
+	provides jynx2asm.ops.MacroLib with
+		jynxmacro.WasmMacroLib,
+		jynxmacro.StructuredMacroLib,
+		jynxmacro.ASMTextMacroLib,
+		jynxmacro.ExtensionMacroLib;
 }
+/*
+.version V11
+.source module-info.java
+.define_module
+.module com.github.david32768.jynxmacro 0.1
+.requires mandated java.base 11
+.requires com.github.david32768.jynx 0.20
+.provides jynx2asm/ops/MacroLib with .array
+  jynxmacro/WasmMacroLib
+  jynxmacro/StructuredMacroLib
+  jynxmacro/ASMTextMacroLib
+  jynxmacro/ExtensionMacroLib
+.end_array
+.packages .array
+  jynxmacro
+.end_array
+; */
